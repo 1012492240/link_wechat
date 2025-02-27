18edfb7fc51b6aa826c5815c87e08a7951ec1357
刘杰 <liljet@ingru.ai>
2025-02-27 14:24:22 +0800

    first commit

---
+++ app/components/WeChatButton.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export const WeChatButton = () => {
+    return (
+        <a
+            href="weixin://"
+            className="bg-green-500 hover:bg-green-600 text-white font-bold py-3 px-8 rounded-full transition-colors duration-300"
+            onClick={(e) => {
+                e.preventDefault();
+                window.location.href = 'weixin://';
+            }}
+        >
+            点击前往微信
+        </a>
+    );
+}; 
